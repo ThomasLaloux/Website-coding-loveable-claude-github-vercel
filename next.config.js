aaca95c2500e94b5f9caf34b5b94779cc318cc3f
ThomasLaloux <thomas_laloux@outlook.com>
2026-05-15 23:41:39 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en', // This means yourdomain.com loads English by default
-  },
-};
-
-module.exports = nextConfig;
